9756701f77ae587bf9ce0ef79a30270896942cf8
Victor Job <victoramalhojob@gmail.com>
2024-09-24 01:37:00 -0300

    Adicionando arquivos do desafio POO

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando o Nokia
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia X1", imei: "111111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Testando o iPhone
+        Smartphone iphone = new Iphone(numero: "7891011", modelo: "iPhone 14", imei: "222222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
